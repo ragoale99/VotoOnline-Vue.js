c73b61678a4e3f9b4dfe0bc4412139f98c61b71c
Raghesh99 <raghesh99@gmail.com>
2021-07-06 17:54:00 +0200

    up

--- src/components/GenericUserContent.vue
+++ src/components/GenericUserContent.vue
@@ -1,143 +1,161 @@
 <template>
 	<div>
-		<v-container fluid class="mt-2">
-			<v-card elevation="24" outlined class="pa-4">
-				<v-card-title class="justify-center pt-0"
-					><h2>Votazioni disponibili</h2>
-				</v-card-title>
-				<v-row class="mx-md-5" align="center">
-					<v-col
-						v-for="votation in votationsToDo"
-						:key="votation.id"
-						cols="12"
-						md="6"
-						lg="4"
-					>
-						<v-card elevation="24" shaped class="px-4 pb-4 borders">
-							<v-card-title class="justify-center"
-								>{{ votation.title }}
-							</v-card-title>
-							<v-card-text class="my-4">
-								<v-row align="center" class="mx-0 mt-2"></v-row>
+		<div v-if="!startVotation">
+			<v-container fluid class="mt-2">
+				<v-card elevation="24" outlined class="pa-4">
+					<v-card-title class="justify-center pt-0"
+						><h2>Votazioni disponibili</h2>
+					</v-card-title>
+					<v-row class="mx-md-5" align="center">
+						<v-col
+							v-for="votation in votationsToDo"
+							:key="votation.id"
+							cols="12"
+							md="6"
+							lg="4"
+						>
+							<v-card elevation="24" shaped class="px-4 pb-4 borders">
+								<v-card-title class="justify-center"
+									>{{ votation.title }}
+								</v-card-title>
+								<v-card-text class="my-4">
+									<v-row align="center" class="mx-0 mt-2"></v-row>
+									<div>
+										Small plates, salads & sandwiches - an intimate setting with
+										12 indoor seats plus patio seating.
+									</div>
+								</v-card-text>
+
 								<div>
-									Small plates, salads & sandwiches - an intimate setting with
-									12 indoor seats plus patio seating.
+									<v-dialog :retain-focus="false" v-model="dialog" width="500">
+										<template v-slot:activator="{ on, attrs }">
+											<v-btn
+												class="mx-4"
+												color="primary"
+												v-bind="attrs"
+												v-on="on"
+												@click="setVotation(votation)"
+												>Dettagli</v-btn
+											>
+										</template>
+
+										<v-card>
+											<v-card-title>
+												<h3>Dettagli votazione</h3>
+											</v-card-title>
+
+											<v-divider></v-divider>
+
+											<v-card-text>
+												<ul class="mt-2">
+													<li>
+														Data di inizio:
+														<strong>
+															{{ dataFormat(selectedVotation.dateStart) }}
+														</strong>
+													</li>
+													<li>
+														Data di inizio:
+														<strong
+															>{{ dataFormat(selectedVotation.dateEnd) }}
+														</strong>
+													</li>
+													<li>
+														Scelte:
+														<ul
+															v-for="option in selectedVotation.options"
+															:key="option.nome"
+														>
+															<div class="flex my-3">
+																<v-avatar height="150" width="150" class="mr-3">
+																	<img
+																		:src="getImgUrl(option.imagePath)"
+																		:alt="option.nome"
+																	/>
+																</v-avatar>
+																<li>
+																	<h2>{{ option.nome }}</h2>
+																</li>
+															</div>
+														</ul>
+													</li>
+												</ul>
+											</v-card-text>
+
+											<v-divider></v-divider>
+
+											<v-card-actions>
+												<v-spacer></v-spacer>
+												<v-btn @click="dialog = false">
+													Indietro
+												</v-btn>
+											</v-card-actions>
+										</v-card>
+									</v-dialog>
+									<v-btn
+										color="success"
+										class="px-4"
+										@click="
+											setVotation(votation);
+											startVotation = true;
+										"
+										>Inizia votazione</v-btn
+									>
 								</div>
-							</v-card-text>
-
-							<div>
-								<v-dialog :retain-focus="false" v-model="dialog" width="500">
-									<template v-slot:activator="{ on, attrs }">
-										<v-btn
-											class="mx-4"
-											color="primary"
-											v-bind="attrs"
-											v-on="on"
-											@click="setVotation(votation)"
-											>Dettagli</v-btn
-										>
-									</template>
-
-									<v-card>
-										<v-card-title>
-											<h3>Dettagli votazione</h3>
-										</v-card-title>
-
-										<v-divider></v-divider>
-
-										<v-card-text>
-											<ul class="mt-2">
-												<li>
-													Data di inizio:
-													<strong>
-														{{ dataFormat(selectedVotation.dateStart) }}
-													</strong>
-												</li>
-												<li>
-													Data di inizio:
-													<strong
-														>{{ dataFormat(selectedVotation.dateEnd) }}
-													</strong>
-												</li>
-												<li>
-													Scelte:
-													<ul
-														v-for="option in selectedVotation.options"
-														:key="option.nome"
-													>
-														<div class="flex my-3">
-															<v-avatar height="150" width="150" class="mr-3">
-																<img
-																	:src="getImgUrl(option.imagePath)"
-																	:alt="option.nome"
-																/>
-															</v-avatar>
-															<li>
-																<h2>{{ option.nome }}</h2>
-															</li>
-														</div>
-													</ul>
-												</li>
-											</ul>
-										</v-card-text>
-
-										<v-divider></v-divider>
-
-										<v-card-actions>
-											<v-spacer></v-spacer>
-											<v-btn @click="dialog = false">
-												Indietro
-											</v-btn>
-										</v-card-actions>
-									</v-card>
-								</v-dialog>
-								<v-btn color="success" class="px-4">Inizia votazione</v-btn>
-							</div>
-						</v-card>
-					</v-col>
-				</v-row></v-card
-			>
-			<v-card elevation="24" outlined class="pa-4 mt-5">
-				<v-card-title class="justify-center pt-0"
-					><h2>Votazioni già concluse</h2>
-				</v-card-title>
-				<v-row class="mx-md-5" align="center">
-					<v-col
-						v-for="votation in votationsDone"
-						:key="votation.id"
-						cols="12"
-						md="6"
-						lg="4"
-					>
-						<v-card elevation="24" outlined shaped class="px-4 pb-4  borders">
-							<v-card-title class="justify-center"
-								>{{ votation.title }}
-							</v-card-title>
-							<v-card-text class="my-4">
-								<v-row align="center" class="mx-0 mt-2"></v-row>
+							</v-card>
+						</v-col>
+					</v-row></v-card
+				>
+				<v-card elevation="24" outlined class="pa-4 mt-5">
+					<v-card-title class="justify-center pt-0"
+						><h2>Votazioni già concluse</h2>
+					</v-card-title>
+					<v-row class="mx-md-5" align="center">
+						<v-col
+							v-for="votation in votationsDone"
+							:key="votation.id"
+							cols="12"
+							md="6"
+							lg="4"
+						>
+							<v-card elevation="24" outlined shaped class="px-4 pb-4  borders">
+								<v-card-title class="justify-center"
+									>{{ votation.title }}
+								</v-card-title>
+								<v-card-text class="my-4">
+									<v-row align="center" class="mx-0 mt-2"></v-row>
+									<div>
+										Small plates, salads & sandwiches - an intimate setting with
+										12 indoor seats plus patio seating.
+									</div>
+								</v-card-text>
 								<div>
-									Small plates, salads & sandwiches - an intimate setting with
-									12 indoor seats plus patio seating.
+									<v-btn class="mx-4" color="primary">Dettagli</v-btn>
 								</div>
-							</v-card-text>
-							<div>
-								<v-btn class="mx-4" color="primary">Dettagli</v-btn>
-							</div>
-						</v-card>
-					</v-col>
-				</v-row></v-card
-			>
-		</v-container>
-		<div class="text-center">
-			<v-dialog v-model="dialog" width="500"> </v-dialog>
+							</v-card>
+						</v-col>
+					</v-row>
+				</v-card>
+			</v-container>
+			<div class="text-center">
+				<v-dialog v-model="dialog" width="500"> </v-dialog>
+			</div>
 		</div>
+		<votation
+			v-if="startVotation"
+			:selected-votation="selectedVotation"
+		></votation>
 	</div>
 </template>
 
 <script>
 import moment from "moment";
+import Votation from "./Votation.vue";
 
 export default {
+	components: {
+		Votation,
+	},
 	data() {
 		return {
 			votations: this.$store.getters.votationsStored,
@@ -145,6 +163,7 @@ export default {
 			selectedVotation: "",
 			votationsToDo: "",
 			votationsDone: "",
+			startVotation: false,
 		};
 	},
 	methods: {
@@ -153,6 +172,7 @@ export default {
 		},
 		setVotation(votation) {
 			this.selectedVotation = votation;
+			console.log(this.selectedVotation);
 		},
 		getImgUrl(image) {
 			return require("../assets/images/" + image);
---
+++ src/components/Votation.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>{{ selectedVotation.title }}</h1>
+</template>
+
+<script>
+export default {
+	props: ["selectedVotation"],
+};
+</script>
