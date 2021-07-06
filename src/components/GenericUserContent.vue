89cc81b0710d440cbe6fd200d5c0d47de726bf19
Raghesh99 <raghesh99@gmail.com>
2021-07-06 14:40:26 +0200

    style

--- src/components/GenericUserContent.vue
+++ src/components/GenericUserContent.vue
@@ -13,7 +13,7 @@
 						md="6"
 						lg="4"
 					>
-						<v-card elevation="24" outlined shaped class="px-4 pb-4">
+						<v-card elevation="24" shaped class="px-4 pb-4 borders">
 							<v-card-title class="justify-center"
 								>{{ votation.title }}
 							</v-card-title>
@@ -36,6 +36,38 @@
 					</v-col>
 				</v-row></v-card
 			>
+			<v-card elevation="24" outlined class="pa-4 mt-5">
+				<v-card-title class="justify-center pt-0"
+					><h2>Votazioni già concluse</h2>
+				</v-card-title>
+				<v-row class="mx-md-5" align="center">
+					<v-col
+						v-for="votation in this.$store.getters.votationsStored"
+						:key="votation.id"
+						cols="12"
+						md="6"
+						lg="4"
+					>
+						<v-card elevation="24" outlined shaped class="px-4 pb-4  borders">
+							<v-card-title class="justify-center"
+								>{{ votation.title }}
+							</v-card-title>
+							<v-card-text class="my-4">
+								<v-row align="center" class="mx-0 mt-2"></v-row>
+								<div>
+									Small plates, salads & sandwiches - an intimate setting with
+									12 indoor seats plus patio seating.
+								</div>
+							</v-card-text>
+							<div>
+								<v-btn @click="deleteItem(item.id)" class="mx-4" color="primary"
+									>Dettagli</v-btn
+								>
+							</div>
+						</v-card>
+					</v-col>
+				</v-row></v-card
+			>
 		</v-container>
 	</div>
 </template>
@@ -50,4 +82,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.borders {
+	border: 1px solid rgb(179, 179, 179);
+}
+</style>
