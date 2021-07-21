77d6807f1546000c13a63bd40d9224e3297a1c9f
Raghesh99 <raghesh99@gmail.com>
2021-07-21 10:07:26 +0200

    scrollbar

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@
 		<the-header></the-header>
 		<v-main>
 			<login-page v-if="getRole === ''"></login-page>
-			<generic-user-content
-				v-if="getRole === 'genericUser'"
-			></generic-user-content>
+			<generic-user-content v-if="getRole === 'genericUser'"></generic-user-content>
 			<admin-content v-if="getRole === 'admin'"></admin-content>
 		</v-main>
 	</v-app>
@@ -45,5 +43,28 @@ body {
 	margin: 0;
 	padding: 0;
 	box-sizing: border-box;
+}
+
+::-webkit-scrollbar {
+	width: 10px;
+	height: 6px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+	box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+	-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+	-webkit-border-radius: 6px;
+	border-radius: 6px;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+	-webkit-border-radius: 6px;
+	border-radius: 6px;
+	background: rgba(149, 231, 195, 0.8);
+	box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+	-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+}
+::-webkit-scrollbar-thumb:window-inactive {
+	background: rgba(221, 196, 196, 0.2);
 }
 </style>
